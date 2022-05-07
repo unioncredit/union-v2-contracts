d9440a416542775893092f238b146588d9e38f20
Gerald <gm@gerald.host>
2022-05-07 19:15:16 +0100

    feat: test wrapper

---
+++ src/test/Wrapper.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import "../interfaces/IUToken.sol";
+import "../interfaces/IUserManager.sol";
+import "../interfaces/IAssetManager.sol";
+import "../interfaces/IInterestRateModel.sol";
+import "../interfaces/IUnionToken.sol";
+import "../interfaces/IComptroller.sol";
+
+contract TestWrapper is Test {
+  IUToken public uToken;
+  IUserManager public userManager;
+  IAssetManager public assetManager;
+  IInterestRateModel public interestRateModel;
+  IUnionToken public unionToken;
+  IComptroller public comptroller;
+
+  function setUp() public {}
+}
