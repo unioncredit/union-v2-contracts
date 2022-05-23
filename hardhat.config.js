58f066d125dbe10322957d7c574e907a6edcba88
Gerald <gm@gerald.host>
2022-05-23 15:09:36 +0100

    feat: init hh tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+    solidity: "0.8.4",
+    paths: {cache: "hh-cache", tests: "./test/hardhat"}
 };
