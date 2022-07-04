0f8161c998d8f8e2e0cbca2a8e6333dcce47a3d1
Gerald <gm@gerald.host>
2022-07-04 23:59:38 +0100

    feat: remove nested testing repo HH

---
+++ test/hardhat/testUserManager.ts
@@ -0,0 +1,7 @@
+import { ethers } from "hardhat";
+
+describe("UserManager.sol", () => {
+    before(async function () {});
+
+    it("empty");
+});
--- test/hardhat/unit/testUserManager.js
+++
@@ -1,8 +0,0 @@
-const {ethers, waffle} = require("hardhat");
-const {expect} = require("chai");
-
-describe("UserManager.sol", () => {
-    before(async function () {});
-
-    it("");
-});
