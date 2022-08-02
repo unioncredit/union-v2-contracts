6482373a75f777aa331b775ea49774d5a7acc482
Gerald <gm@gerald.host>
2022-08-02 16:35:12 +0100

    feat: added tests for comptroller internals

--- test/foundry/comptroller/TestComptrollerBase.sol
+++ test/foundry/comptroller/TestComptrollerBase.sol
@@ -3,8 +3,25 @@ pragma solidity ^0.8.0;
 import {TestWrapper} from "../TestWrapper.sol";
 import {Comptroller} from "union-v1.5-contracts/token/Comptroller.sol";
 
+contract ComptrollerInternals is Comptroller {
+    function getInflationIndex(
+        uint256 effectiveAmount,
+        uint256 inflationIndex,
+        uint256 blockDelta
+    ) public view returns (uint256) {
+        return _getInflationIndex(effectiveAmount, inflationIndex, blockDelta);
+    }
+
+    function lookup(uint256 index) public pure returns (uint256) {
+        return _lookup(index);
+    }
+}
+
 contract TestComptrollerBase is TestWrapper {
     Comptroller public comptroller;
+    ComptrollerInternals public comptrollerInternals;
+
+    uint256 public halfDecayPoint = 1000000;
 
     function setUp() public virtual {
         address logic = address(new Comptroller());
@@ -14,6 +31,22 @@ contract TestComptrollerBase is TestWrapper {
         uint256 halfDecayPoint = 1000000;
 
         comptroller = Comptroller(
+            deployProxy(
+                logic,
+                abi.encodeWithSignature(
+                    "__Comptroller_init(address,address,uint256)",
+                    unionTokenMock,
+                    marketRegistryMock,
+                    halfDecayPoint
+                )
+            )
+        );
+    }
+
+    function deployComtrollerExposedInternals() public {
+        address logic = address(new ComptrollerInternals());
+
+        comptrollerInternals = ComptrollerInternals(
             deployProxy(
                 logic,
                 abi.encodeWithSignature(
