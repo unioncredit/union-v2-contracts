0f91176551f5cd3691816e849c86737fe9ed4eff
Gerald <gm@gerald.host>
2022-08-10 13:44:02 +0100

    feat: userManager admin check tests

--- test/integration/admin.ts
+++ test/integration/admin.ts
@@ -9,11 +9,13 @@ describe("Owner/Admin permissions", () => {
     let signers: Signer[];
     let deployer: Signer;
     let deployerAddress: string;
+    let nonAdmin: Signer;
     let contracts: Contracts;
 
     before(async function () {
         signers = await ethers.getSigners();
         deployer = signers[0];
+        nonAdmin = signers[1];
         deployerAddress = await deployer.getAddress();
     });
 
@@ -23,7 +25,30 @@ describe("Owner/Admin permissions", () => {
 
     context("UserManager", () => {
         before(beforeContext);
-        it("<FUNCTION> cannot be called by non owner");
+        it("setMaxStakeAmount cannot be called by non owner", async () => {
+            const resp = contracts.userManager.connect(nonAdmin).setMaxStakeAmount(0);
+            await expect(resp).to.be.revertedWith("Controller: not admin");
+        });
+        it("setUToken cannot be called by non owner", async () => {
+            const resp = contracts.userManager.connect(nonAdmin).setUToken(deployerAddress);
+            await expect(resp).to.be.revertedWith("Controller: not admin");
+        });
+        it("setNewMemberFee cannot be called by non owner", async () => {
+            const resp = contracts.userManager.connect(nonAdmin).setNewMemberFee(0);
+            await expect(resp).to.be.revertedWith("Controller: not admin");
+        });
+        it("setMaxOverdueBlocks cannot be called by non owner", async () => {
+            const resp = contracts.userManager.connect(nonAdmin).setMaxOverdueBlocks(0);
+            await expect(resp).to.be.revertedWith("Controller: not admin");
+        });
+        it("setEffectiveCount cannot be called by non owner", async () => {
+            const resp = contracts.userManager.connect(nonAdmin).setEffectiveCount(0);
+            await expect(resp).to.be.revertedWith("Controller: not admin");
+        });
+        it("addMember cannot be called by non owner", async () => {
+            const resp = contracts.userManager.connect(nonAdmin).addMember(deployerAddress);
+            await expect(resp).to.be.revertedWith("Controller: not admin");
+        });
     });
 
     context("UToken", () => {
