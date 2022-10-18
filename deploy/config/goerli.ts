605fc82610a5334665e2956c99aa2241c648ff74
twygod <twygod@gmail.com>
2022-10-18 14:35:04 +0800

    new: deploy on goerli.

--- deploy/config/goerli.ts
+++ deploy/config/goerli.ts
@@ -3,6 +3,10 @@ import {DeployConfig} from "../index";
 export default {
     addresses: {
         unionToken: "0x23B0483E07196c425d771240E81A9c2f1E113D3A",
-        dai: "0xdc31ee1784292379fbb2964b3b9c4124d8f89c60" // DAI used by compound on goerli
+        dai: "0xDF1742fE5b0bFc12331D8EAec6b478DfDbD31464", // DAI used by aave on goerli
+        aave: {
+            market: "0x0C501fB73808e1BD73cBDdd0c99237bbc481Bb58",
+            lendingPool: "0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6"
+        }
     }
 } as DeployConfig;
