1617eafb60e13fd84ed954d7d07327f01a4ba59e
twygod <twygod@gmail.com>
2023-02-08 16:58:43 +0800

    chg: deploy opConnector on goerli.

--- deploy/config/goerli.ts
+++ deploy/config/goerli.ts
@@ -9,6 +9,6 @@ export default {
             lendingPool: "0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6"
         },
         opL1Bridge: "0x636Af16bf2f682dD3109e60102b8E1A089FedAa8",
-        opUnion: "0x04622FDe6a7C0B19Ff7748eC8882870367530074"
+        opUnion: "0xe8281FdF8945E06C608b1C95D8f6dCEDbf2AC323"
     }
 } as DeployConfig;
