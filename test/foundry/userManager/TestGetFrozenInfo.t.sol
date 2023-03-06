7a21d3d2253eb6a8feedf095c193d42dbc41cdd9
Gerald <gm@gerald.host>
2023-03-06 15:00:31 +0000

    feat: user manager coverage

--- test/foundry/userManager/TestGetFrozenInfo.t.sol
+++ test/foundry/userManager/TestGetFrozenInfo.t.sol
@@ -21,6 +21,10 @@ contract TestGetFrozenInfo is TestUserManagerBase {
     function testGetStakeInfo() public {
         uint96 lockAmount = 10 ether;
         uint256 creditLimit = userManager.getCreditLimit(ACCOUNT);
+
+        uint256 globalStakedBefore = userManager.globalTotalStaked();
+        uint256 totalFrozenBefore = userManager.totalFrozen();
+
         vm.assume(lockAmount <= creditLimit);
         vm.roll(11); // 10 more blocks
         vm.startPrank(address(userManager.uToken()));
@@ -31,5 +35,9 @@ contract TestGetFrozenInfo is TestUserManagerBase {
         assertEq(effectiveStaked, stakeAmount);
         // lockAmount/2 because only locked for half of the duration
         assertEq(effectiveLocked, lockAmount / 2);
+
+        uint256 globalStakedAfter = userManager.globalTotalStaked();
+        uint256 totalFrozenAfter = userManager.totalFrozen();
+        assertEq(globalStakedBefore - globalStakedAfter, totalFrozenAfter - totalFrozenBefore);
     }
 }
