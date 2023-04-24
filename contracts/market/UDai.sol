f00fad0ca83fe570e3c5f9b5a8b4b6919e0bba60
Max Weng <max.m.weng@gmail.com>
2023-04-24 23:53:19 +0800

    chg: remove repayInterestWithPermit()

--- contracts/market/UDai.sol
+++ contracts/market/UDai.sol
@@ -22,21 +22,4 @@ contract UDai is UToken, IUDai {
         uint256 interest = calculatingInterest(borrower);
         _repayBorrowFresh(msg.sender, borrower, amount, interest);
     }
-
-    function repayInterestWithPermit(
-        address borrower,
-        uint256 nonce,
-        uint256 expiry,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external whenNotPaused {
-        IDai erc20Token = IDai(underlying);
-        erc20Token.permit(msg.sender, address(this), nonce, expiry, true, v, r, s);
-
-        if (!accrueInterest()) revert AccrueInterestFailed();
-        uint256 interest = calculatingInterest(borrower);
-        _repayBorrowFresh(msg.sender, borrower, interest, interest);
-    }
-
 }
