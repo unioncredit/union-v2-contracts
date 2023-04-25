5da0b325717c09a9055ecd683096f8a0511223fb
Max Weng <max.m.weng@gmail.com>
2023-04-25 10:37:55 +0800

    chg: update DAI address

--- deploy/config/optimism.ts
+++ deploy/config/optimism.ts
@@ -4,7 +4,7 @@ export default {
     addresses: {
         guardian: "0xF7dc916eC6ee854b3a32f5D8DcF2ED0582e05Dc3",
         unionToken: "0x5Dfe42eEA70a3e6f93EE54eD9C321aF07A85535C",
-        dai: "0x6b175474e89094c44da98b954eedeac495271d0f",
+        dai: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
         aave: {
             market: "0x929EC64c34a17401F460460D4B9390518E5B473e",
             lendingPool: "0x794a61358d6845594f94dc1db02a252b5b4814ad"
