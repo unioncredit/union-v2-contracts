632d7fb4f57e0b99355949bdf905ddda0fecd5bf
Max Weng <max.m.weng@gmail.com>
2023-04-25 12:34:43 +0800

    chg: add opConnector deployment

--- deploy/config/mainnet.ts
+++ deploy/config/mainnet.ts
@@ -4,6 +4,6 @@ export default {
     addresses: {
         unionToken: "0x5Dfe42eEA70a3e6f93EE54eD9C321aF07A85535C",
         opUnion: "0xB025ee78b54B5348BD638Fe4a6D77Ec2F813f4f9",
-        opL1Bridge: ""
+        opL1Bridge: "0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1"
     }
 } as DeployConfig;
