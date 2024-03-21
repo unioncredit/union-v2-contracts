77582956d868de33b6b047831f9df9c3b652c833
twygod <twygod@gmail.com>
2024-03-21 14:11:00 +0800

    fix: unit error

--- utils/index.ts
+++ utils/index.ts
@@ -2,5 +2,5 @@ import {BigNumber} from "ethers";
 import {parseUnits} from "ethers/lib/utils";
 
 export const parseUSDC = (value: string): BigNumber => {
-    return parseUnits(value, 8);
+    return parseUnits(value, 6);
 };
