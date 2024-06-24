5b204baf5f57bb5c1ef9b860683ef862c01adafa
twygod <twygod@gmail.com>
2024-06-24 17:50:03 +0800

    fix: update erc20 mock contract to fix permit func error

--- contracts/mocks/FaucetERC20.sol
+++ contracts/mocks/FaucetERC20.sol
@@ -2,12 +2,14 @@
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 // import {Test} from "forge-std/Test.sol";
 
-contract FaucetERC20 is ERC20 {
+contract FaucetERC20 is ERC20, ERC20Permit {
     uint8 public _decimals = 18;
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {}
 
     function mint(address to, uint256 value) public returns (bool) {
         // require(value <= 10000000 ether, "dont be greedy");
@@ -18,6 +20,10 @@ contract FaucetERC20 is ERC20 {
     function burn(address to, uint256 value) public returns (bool) {
         _burn(to, value);
         return true;
+    }
+
+    function burnFrom(address account, uint256 amount) public virtual {
+        _burn(account, amount);
     }
 
     function decimals() public view override returns (uint8) {
