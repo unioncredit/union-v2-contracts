a01f6fc60c408670e21bdeb4962ac3c6bc5c2cc9
Max Weng <max.m.weng@gmail.com>
2024-12-10 13:43:09 +0800

    chg: add getTotalCredit()

--- contracts/interfaces/IUserManager.sol
+++ contracts/interfaces/IUserManager.sol
@@ -158,4 +158,6 @@ interface IUserManager {
      *  @param amount Amount
      */
     function unstake(uint96 amount) external;
+
+    function stakingTokenDecimal() external view returns (uint8);
 }
